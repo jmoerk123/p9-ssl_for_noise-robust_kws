931c0dff8f959ade0cd4b5ab3ec50044f5636e2b
jmoerk123 <jmoerk0101@gmail.com>
2023-10-13 12:13:45 +0200

    created create_mixed_noise_files.py

---
+++ create_mixed_files/create_mixed_files_busxppl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# busxppl
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr-10 -o2 $HOME/data/bus_google_speech_commands/snr-10 -s $HOME/data/mixed_data/busxppl/snr-10
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr-5 -o2 $HOME/data/bus_google_speech_commands/snr-5 -s $HOME/data/mixed_data/busxppl/snr-5
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr0 -o2 $HOME/data/bus_google_speech_commands/snr0 -s $HOME/data/mixed_data/busxppl/snr0
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr5 -o2 $HOME/data/bus_google_speech_commands/snr5 -s $HOME/data/mixed_data/busxppl/snr5
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr10 -o2 $HOME/data/bus_google_speech_commands/snr10 -s $HOME/data/mixed_data/busxppl/snr10
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr15 -o2 $HOME/data/bus_google_speech_commands/snr15 -s $HOME/data/mixed_data/busxppl/snr15
+
+python create_mixed_files/create_mixed_noise_files.py -o1 $HOME/data/bus_google_speech_commands/snr20 -o2 $HOME/data/bus_google_speech_commands/snr20 -s $HOME/data/mixed_data/busxppl/snr20
