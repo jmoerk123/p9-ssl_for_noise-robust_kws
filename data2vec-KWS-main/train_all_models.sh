9e2f9c9218b274155cf1b93b8ebcdaade230c504
jmoerk123 <jmoerk0101@gmail.com>
2023-10-19 13:49:29 +0200

    new configs

--- data2vec-KWS-main/train_all_models.sh
+++ data2vec-KWS-main/train_all_models.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Baselines
-python  projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/kwt1_baseline_mean_config.yaml
+# python  projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/kwt1_baseline_mean_config.yaml
 # python train.py --conf KWT_configs/kwt2_baseline_mean_config.yaml
 # python train.py --conf KWT_configs/kwt3_baseline_mean_config.yaml
 
 # Data2Vec pretraining
-# python train_data2vec.py --conf data2vec/data2vec_configs/kwt1_data2vec_config.yaml
-# python train_data2vec.py --conf data2vec/data2vec_configs/kwt2_data2vec_config.yaml
-# python train_data2vec.py --conf data2vec/data2vec_configs/kwt3_data2vec_config.yaml
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train_data2vec.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/data2vec/data2vec_configs/kwt1_data2vec_config.yaml
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train_data2vec.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/data2vec/data2vec_configs/kwt2_data2vec_config.yaml
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train_data2vec.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/data2vec/data2vec_configs/kwt3_data2vec_config.yaml
 
 # # Fine-tuning
-# python train.py --conf KWT_configs/kwt1_finetune_mean_config.yaml --ckpt runs/kwt1_data2vec/best_encoder.pth
-# python train.py --conf KWT_configs/kwt2_finetune_mean_config.yaml --ckpt runs/kwt2_data2vec/best_encoder.pth
-# python train.py --conf KWT_configs/kwt3_finetune_mean_config.yaml --ckpt runs/kwt3_data2vec/best_encoder.pth
+# python projects/p9-ssl_for_noise-robust_kws/train.py --conf projects/p9-ssl_for_noise-robust_kws/KWT_configs/kwt1_finetune_mean_config.yaml --ckpt data/runs/kwt1_data2vec_pre_nonoise/best.pth
+# python projects/p9-ssl_for_noise-robust_kws/train.py --conf projects/p9-ssl_for_noise-robust_kws/KWT_configs/kwt2_finetune_mean_config.yaml --ckpt data/runs/kwt2_data2vec_pre_nonoise/best.pth
+# python projects/p9-ssl_for_noise-robust_kws/train.py --conf projects/p9-ssl_for_noise-robust_kws/KWT_configs/kwt3_finetune_mean_config.yaml --ckpt data/runs/kwt3_data2vec_pre_nonoise/best.pth
