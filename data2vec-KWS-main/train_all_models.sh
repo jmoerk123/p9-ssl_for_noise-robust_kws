0674049dfb4ba5123577396c443817ccabd1459e
jmoerk123 <jmoerk0101@gmail.com>
2023-10-24 12:58:14 +0200

    changed best to best_encoder for finetuning

--- data2vec-KWS-main/train_all_models.sh
+++ data2vec-KWS-main/train_all_models.sh
@@ -11,6 +11,6 @@
 # python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train_data2vec.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/data2vec/data2vec_configs/kwt3_data2vec_config.yaml
     
 # # Fine-tuning
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt1_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt1_data2vec_pre_nonoise/best.pth
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt2_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt2_data2vec_pre_nonoise/best.pth
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt3_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt3_data2vec_pre_nonoise/best.pth
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt1_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt1_data2vec_pre_nonoise/best_encoder.pth
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt2_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt2_data2vec_pre_nonoise/best_encoder.pth
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/noise/kwt3_finetune_mean_config_snr-5.yaml --ckpt data/runs/kwt3_data2vec_pre_nonoise/best_encoder.pth
