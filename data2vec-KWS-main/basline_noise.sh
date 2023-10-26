505df53bd766df73c45495b1d995da2f0fb72920
jmoerk123 <jmoerk0101@gmail.com>
2023-10-26 08:22:05 +0200

    Removed /home/jacobuni from all configs

--- data2vec-KWS-main/basline_noise.sh
+++ data2vec-KWS-main/basline_noise.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 # Fine-tuning snr-10
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr-10.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr-10.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr-10.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr-10.yaml 
 
 # Fine-tuning snr-5
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr-5.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr-5.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr-5.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr-5.yaml 
 
 # Fine-tuning snr0
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr0.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr0.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr0.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr0.yaml 
 
 # Fine-tuning snr5
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr5.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr5.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr5.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr5.yaml 
 
 # Fine-tuning snr10
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr10.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr10.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr10.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr10.yaml 
 
 # Fine-tuning snr15
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr15.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr15.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr15.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr15.yaml 
 
 # Fine-tuning snr20
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr20.yaml 
+# python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt1_baseline_mean_config_snr20.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt2_baseline_mean_config_snr20.yaml 
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/baseline_noise/kwt3_baseline_mean_config_snr20.yaml 
