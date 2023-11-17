dd25e12509c5fee60292c59612bb82734d534fc5
jmoerk123 <jmoerk0101@gmail.com>
2023-11-17 08:31:16 +0100

    fixed train_multidata.sh

--- data2vec-KWS-main/train_multidata.sh
+++ data2vec-KWS-main/train_multidata.sh
@@ -7,5 +7,5 @@
 
 # Finetune pretrained multidate
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/multidata/kwt1_finetune_mean_config_multidata.yaml --ckpt data/runs/kwt1_data2vec_multidata/best_encoder.pth
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/multidata/kwt1_finetune_mean_config_multidata.yaml --ckpt data/runs/kwt2_data2vec_multidata/best_encoder.pth
-python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/multidata/kwt1_finetune_mean_config_multidata.yaml --ckpt data/runs/kwt3_data2vec_multidata/best_encoder.pth
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/multidata/kwt2_finetune_mean_config_multidata.yaml --ckpt data/runs/kwt2_data2vec_multidata/best_encoder.pth
+python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/multidata/kwt3_finetune_mean_config_multidata.yaml --ckpt data/runs/kwt3_data2vec_multidata/best_encoder.pth
