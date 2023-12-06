e8f33532d16654ef570f1833e2554906fb463362
jmoerk123 <jmoerk0101@gmail.com>
2023-12-06 19:01:50 +0100

    fix i hope

--- data2vec-KWS-main/utils/dataset.py
+++ data2vec-KWS-main/utils/dataset.py
@@ -103,9 +103,9 @@ class GoogleSpeechDataset(Dataset):
             self.label_list = []
             label_2_idx = {v: int(k) for k, v in label_map.items()}
             for path in data_list:
-                if path != "":
-                    print(f'path: {path}')
-                    self.label_list.append(label_2_idx[Path(path).parts[-2]])
+                # if path != "":
+                #     print(f'path: {path}')
+                self.label_list.append(label_2_idx[Path(path).parts[-2]])
         else:
             self.label_list = None
 
