95594ba7db8fa8832dbdb8f904e04c8768b1d079
jmoerk123 <jmoerk0101@gmail.com>
2023-12-06 08:27:31 +0100

    new training ready (multidata)

--- data2vec-KWS-main/train_noise_snrmix+clean.sh
+++ data2vec-KWS-main/train_noise_snrmix+clean.sh
@@ -25,7 +25,7 @@ python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf pr
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/mixed_snr_and_clean/kwt2_finetune_mean_config_prenonoise_snrmix+clean.yaml --ckpt data/runs/kwt2_data2vec_pre_nonoise/best_encoder.pth
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/mixed_snr_and_clean/kwt3_finetune_mean_config_prenonoise_snrmix+clean.yaml --ckpt data/runs/kwt3_data2vec_pre_nonoise/best_encoder.pth
 
-# snrmix finetune no prenoise and no fintune noise
+# finetune no prenoise and no fintune noise
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/mixed_snr_and_clean/kwt1_baseline_finetune_mean_config_clean.yaml --ckpt data/runs/kwt1_data2vec_pre_nonoise/best_encoder.pth
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/mixed_snr_and_clean/kwt2_baseline_finetune_mean_config_clean.yaml --ckpt data/runs/kwt2_data2vec_pre_nonoise/best_encoder.pth
 python projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/train.py --conf projects/p9-ssl_for_noise-robust_kws/data2vec-KWS-main/KWT_configs/mixed_snr_and_clean/kwt3_baseline_finetune_mean_config_clean.yaml --ckpt data/runs/kwt3_data2vec_pre_nonoise/best_encoder.pth
